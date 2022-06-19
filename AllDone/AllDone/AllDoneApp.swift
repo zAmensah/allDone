6807af9a8598fb6029d1eab6f631b9674ec65365
Albert Mensah <zamensah840@gmail.com>
2022-06-19 17:29:45 +0000

    project setup

---
+++ AllDone/AllDone/AllDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneApp.swift
+//  AllDone
+//
+//  Created by Albert Mensah on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AllDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
